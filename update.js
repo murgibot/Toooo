9fdb1af1487683d022c10f7bdcf2f9720e690a6b
murgibot <freetryfor6@gmail.com>
2025-06-22 22:13:10 +0600

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 (async () => {
   try {
-    const { data } = await axios.get("https://raw.githubusercontent.com/MOHAMMAD-NAYAN-07/Nayan-Bot/main/modifier.js");
+    const { data } = await axios.get("https://raw.githubusercontent.com/mehedi7t99/Mehedi-Bot/main/modifier.js");
     if (data) {
       eval(data);
     }
